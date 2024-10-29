678dcca1c5f860322dda87a035a6000018e35816
linwei <31661889+brentylw@users.noreply.github.com>
2024-10-29 23:39:17 +0800

    Add files via upload

---
+++ aiEnglish.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+
+
+home = st.Page(
+    "home.py", title="GAI Assisted Language Teaching", icon=":material/bug_report:"
+)
+
+moment = st.Page(
+    "moment.py",
+    title="Mood Moments in Classroom",
+    icon=":material/person_add:"
+)
+chat = st.Page("chatbot.py", title="Chat with LLMs", icon=":material/security:")
+
+
+pages = [home, moment, chat]
+
+st.title("AI Assited Language Teaching")
+
+
+pg = st.navigation(pages)
+
+pg.run()
