5250771c568fb044af12e6b62a4389e4fcc5ee43
linwei <31661889+brentylw@users.noreply.github.com>
2024-11-10 05:01:01 +0800

    Update essayrater.py

--- essayrater.py
+++ essayrater.py
@@ -4,6 +4,7 @@ from io import BytesIO
 from PIL import Image
 import base64
 import baiduocr
+
 api_key = "sk-umH9cf34da865f51e5b07d829a28bac29d8cac32594pvwsD"
 if 'history' not in st.session_state:
     st.session_state['history'] = [{'role': 'system', 'content': ''}]
@@ -50,47 +51,4 @@ st.markdown(
 )
 cols = st.columns(2)
 
-# send api request
-with cols[0]:
-    if st.button('Send'):
-        if not api_key:
-            st.warning('API Key required')
-            st.stop()
-        if not (text_input or img_input):
-            st.warning('You can\'t just send nothing!')
-            st.stop()
-        msg = {'role': 'user', 'content': []}
-        if text_input:
-            msg['content'].append({'type': 'text', 'text': text_input})
-        for img in img_input:
-            if img.name.split('.')[-1].lower() not in ['png', 'jpg', 'jpeg', 'gif', 'webp']:
-                st.warning('Only .jpg, .png, .gif, or .webp are supported')
-                st.stop()
-            encoded_img = base64.b64encode(img.read()).decode('utf-8')
-            result = baiduocr.ocr_image(encoded_img)
-            msg['content'].append(result)
-            
-        st.session_state['history'].append(msg)
-        history = (
-            st.session_state['history']
-            if st.session_state['history'][0]['content']
-            else st.session_state['history'][1:]
-        )
 
-        client = OpenAI(base_url="https://api.gptsapi.net/v1", api_key=api_key)
-            response = client.chat.completions.create(
-                model = "gpt-4o",
-                messages=history
-            )
-        
-
-        st.session_state['history'].append(
-            {'role': 'assistant', 'content': result}
-        )
-        st.rerun()
-
-# clear chat history
-with cols[1]:
-    if st.button('Clear'):
-        st.session_state['history'] = [st.session_state['history'][0]]
-        st.rerun()
