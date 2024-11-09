6ae616d390e3d46f0679e1ee8af3237c69e8d022
linwei <31661889+brentylw@users.noreply.github.com>
2024-11-10 04:19:19 +0800

    Update home.py

--- home.py
+++ home.py
@@ -1,15 +1,9 @@
 import streamlit as st
 st.caption("🚀 welcome")
-st.title("About")
+st.title("徐州 江苏师范大学")
 
 st.write("""
-This is designed to provide language teachers and learners an interactive platform to learn English. 
-The bot uses a combination of advanced natural language processing algorithms to simulate a conversation, 
-provide feedback on your proficiency, grammar, and vocabulary usage.
+新时代学科教学（英语）专业硕士人才培养研讨会
 
-**Note:** This tool is intended for practice and should not be used as a sole method for learning a language. 
-The bot's feedback is based on algorithms and may not always reflect human judgment.
-
-If you have any feedback or suggestions, please let me know.
 """)
 st.write("Your Language Learning Assistant. Created by a Language Enthusiasts for Language Learners.")
