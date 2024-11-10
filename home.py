d329c6b327e8f13a709603296763eda7592488c4
linwei <31661889+brentylw@users.noreply.github.com>
2024-11-10 13:17:54 +0800

    Update home.py

--- home.py
+++ home.py
@@ -6,9 +6,5 @@ st.write("""
 新时代学科教学（英语）专业硕士人才培养研讨会
 
 """)
-st.download_button(
-        label="Download case_data",
-        data=csv_data,
-        file_name='case_data.rar'
-    )
+
 st.write("Your Language Learning Assistant. By Linwei Yang at Yantai University")
